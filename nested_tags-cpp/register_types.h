d7fe13aab4bc8c056b70626e9bc6fd4e9b756921
Arokh Slade <arokhslade@gmail.com>
2026-07-19 09:02:26 +0200

    [add] build script (scons). register_types.

---
+++ nested_tags-cpp/register_types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_nested_tags_module(ModuleInitializationLevel p_level);
+void uninitialize_nested_tags_module(ModuleInitializationLevel p_level);
