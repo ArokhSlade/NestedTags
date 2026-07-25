e9089125bfc6f6e54a6523a712d0abf548fc4375
Arokh Slade <arokhslade@gmail.com>
2026-07-25 17:25:39 +0200

    [fix/cleanup] NestedTags: working project settings menu to set up Definition
    todo/bug: opening the resource picker causes an error "trying to open another exclusive window"
    looks like it's somehow illegal to have a resource picker open while project settings are open

--- nested_tags-godot/addons/nested_tags/ui/nested_tags_plugin_settings.gd
+++ nested_tags-godot/addons/nested_tags/ui/nested_tags_plugin_settings.gd
@@ -1,47 +1,31 @@
 @tool extends Control
-
-var picker
 var object
 
-@export var resource := NestedTagsDefinition.new():
-	set(value):
-		resource = value
-		# Rebuild the UI automatically whenever the object changes
-		show_resource_edit()
-@onready var vbox = $VBoxContainer/ScrollContainer/VBoxContainer
+@export var resource : NestedTagsDefinition
 
 func _ready():
-	#picker = EditorResourcePicker.new()
-	#picker.base_type = "NestedTagsDefinition"
-	#%PickerParent.add_child(picker)
 	show_resource_edit()
-	object = Node2D.new()
-	add_child(object)
-	$VBoxContainer/ScrollContainer.target_object = self
+
 
 func show_resource_edit():
 	var obj_id = self.get_instance_id()
-	var editor_property: EditorProperty = EditorInterface.get_inspector().instantiate_property_editor(
+	
+	var resource_picker = EditorInterface.get_inspector().instantiate_property_editor(
 		self, 
 		TYPE_OBJECT, 
 		"resource", 
 		PROPERTY_HINT_RESOURCE_TYPE, 
-		"", 
+		"NestedTagsDefinition", 
 		PROPERTY_USAGE_DEFAULT
 	)
 	
-	if editor_property:
-		editor_property.label = editor_property.name
-		
-		# Assign the pre-calculated integer instance ID safely
-		editor_property.set_object_and_property(self, "resource")
-		
-		# Bind synchronization logic using a bound argument to avoid lambda capture leakage
-		editor_property.property_changed.connect(
-			func(property, value, field, changing):
-				if is_instance_valid(self):
-					self.set(property, value)
-		)
-		
-		vbox.add_child(editor_property)
-		editor_property.update_property() 
+	resource_picker.label = resource_picker.name
+	resource_picker.set_object_and_property(self, "resource")
+	
+	resource_picker.property_changed.connect(
+		func(property, value, field, changing):
+			self.set(property, value)
+	)
+	
+	%UIParent.add_child(resource_picker)
+	resource_picker.update_property() 
