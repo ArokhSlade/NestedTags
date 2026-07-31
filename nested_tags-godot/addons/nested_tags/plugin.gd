f44e4e43c70fce1e7ca626e3ed4769e21f2de271
Arokh Slade <arokhslade@gmail.com>
2026-07-31 20:35:06 +0200

    [fix] Plugin Initialization: load non-autoload components on enter_tree rather than enable_plugin.

--- nested_tags-godot/addons/nested_tags/plugin.gd
+++ nested_tags-godot/addons/nested_tags/plugin.gd
@@ -8,20 +8,19 @@ var nested_tags_plugin_settings
 
 func _enable_plugin():
 	add_autoload_singleton("NestedTags", "./nested_tags.tscn")
+
+
+func _disable_plugin():
+	remove_autoload_singleton("NestedTags")
+	
+
+func _enter_tree():
 	nested_tag_inspector = NESTED_TAG_INSPECTOR.new()
 	add_inspector_plugin(nested_tag_inspector)
 	nested_tags_plugin_settings = NESTED_TAGS_PLUGIN_SETTINGS.instantiate()
 	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings)
 
-func _disable_plugin():
-	remove_autoload_singleton("NestedTags")
-	remove_inspector_plugin(nested_tag_inspector)
-	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings)
-
-func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
-
 
 func _exit_tree():
-	pass
+	remove_inspector_plugin(nested_tag_inspector)
+	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings)
