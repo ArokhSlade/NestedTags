3f74de28dbb0305332d35b82943293742f65d522
Arokh Slade <139560570+ArokhSlade@users.noreply.github.com>
2026-09-15 13:05:01 +0200

    [WIP/refactor] singleton with settings is no longer global.
    MVC pattern like previously:
    plugin settings = VC
    nested tags = M

--- nested_tags-godot/addons/nested_tags/plugin.gd
+++ nested_tags-godot/addons/nested_tags/plugin.gd
@@ -6,12 +6,16 @@ var nested_tag_inspector
 const NESTED_TAGS_PLUGIN_SETTINGS_TAB = preload("uid://bou2s5kt4dbb")
 var nested_tags_plugin_settings_tab
 
-const NestedTagsDefinitionInspector = preload("uid://ds5nxhwe3g0a8")
+const NESTED_TAGS_DEFINITION_INSPECTOR = preload("uid://ds5nxhwe3g0a8")
 var nested_tags_definition_inspector
+
+const NESTED_TAGS = preload("uid://cqm1sstepwtis")
+var nested_tags
 
 
 func _enable_plugin():
-	pass
+	nested_tags_plugin_settings_tab.initialize(nested_tags)
+	nested_tags_plugin_settings_tab.refresh()
 
 
 func _disable_plugin():
@@ -22,21 +26,17 @@ func _enter_tree():
 	nested_tag_inspector = NestedTagInspector.new()
 	add_inspector_plugin(nested_tag_inspector)
 	
-	nested_tags_definition_inspector = NestedTagsDefinitionInspector.new()
+	nested_tags_definition_inspector = NESTED_TAGS_DEFINITION_INSPECTOR.new()
 	add_inspector_plugin(nested_tags_definition_inspector)
 	
 	nested_tags_plugin_settings_tab = NESTED_TAGS_PLUGIN_SETTINGS_TAB.instantiate()
 	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings_tab)
 	
-	add_autoload_singleton("NestedTags", "./nested_tags.tscn")
-	nested_tags_plugin_settings_tab.refresh()
-	
-	print("enter tree")
+	nested_tags = NESTED_TAGS.instantiate()
+	add_child(nested_tags)
 
 
 func _exit_tree():
 	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings_tab)
 	remove_inspector_plugin(nested_tags_definition_inspector)
 	remove_inspector_plugin(nested_tag_inspector)
-	
-	remove_autoload_singleton("NestedTags")
--- nested_tags-godot/addons/nested_tags/ui/nested_tags_plugin_settings_tab.gd
+++ nested_tags-godot/addons/nested_tags/ui/nested_tags_plugin_settings_tab.gd
@@ -1,7 +1,10 @@
 @tool extends Control
-var object
 
-var definition : NestedTagsDefinition
+const NestedTags = preload("uid://cccga8o21pfq5")
+
+var _definition : NestedTagsDefinition
+var _nested_tags : NestedTags
+
 @onready var editor = %NestedTagsDefinitionEditor
 
 
@@ -9,12 +12,16 @@ func _on_button_pressed():
 	%EditorFileDialog.popup_centered()
 
 
+func initialize(nested_tags):
+	_nested_tags = nested_tags
+
+
 func refresh():
-	definition = NestedTags.get_definition()
-	editor.set_definition(definition)
+	_definition = _nested_tags.get_definition()
+	editor.set_definition(_definition)
 
 
 func _on_editor_file_dialog_file_selected(path):
-	NestedTags.load_definition(path)
-	NestedTags.store_project_setting(path)
+	_nested_tags.load_definition(path)
+	_nested_tags.store_project_setting(path)
 	refresh()
