2e94ccebc985459cadbf3912d32ab0e3dc18ca19
Arokh Slade <139560570+ArokhSlade@users.noreply.github.com>
2026-09-15 08:28:39 +0200

    [rename] NestedTagsPluginSettingsTab, in preparation to differentiate model from view

--- nested_tags-godot/addons/nested_tags/plugin.gd
+++ nested_tags-godot/addons/nested_tags/plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 const NestedTagInspector = preload("uid://45fgmuqhruwy")
 var nested_tag_inspector
 
-const NESTED_TAGS_PLUGIN_SETTINGS = preload("uid://bou2s5kt4dbb")
-var nested_tags_plugin_settings
+const NESTED_TAGS_PLUGIN_SETTINGS_TAB = preload("uid://bou2s5kt4dbb")
+var nested_tags_plugin_settings_tab
 
 const NestedTagsDefinitionInspector = preload("uid://ds5nxhwe3g0a8")
 var nested_tags_definition_inspector
@@ -33,11 +33,11 @@ func _enter_tree():
 	nested_tags_definition_inspector = NestedTagsDefinitionInspector.new()
 	add_inspector_plugin(nested_tags_definition_inspector)
 	
-	nested_tags_plugin_settings = NESTED_TAGS_PLUGIN_SETTINGS.instantiate()
-	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings)
+	nested_tags_plugin_settings_tab = NESTED_TAGS_PLUGIN_SETTINGS_TAB.instantiate()
+	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings_tab)
 
 
 func _exit_tree():
 	remove_inspector_plugin(nested_tag_inspector)
 	remove_inspector_plugin(nested_tags_definition_inspector)
-	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings)
+	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, nested_tags_plugin_settings_tab)
